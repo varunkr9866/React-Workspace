8328c8aac85d7c81d69b342004867ff866e92bc1
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-19 13:11:09 +0530

    COMPLETED NAVBAR AND WORKING ON PROPS BY REACT

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  let name = "Varun Reddy";
+  return (
+    <>
+      <Navbar title = "TextUtils" aboutText = "About"/>
+    </>
+  );
+}
+
+export default App;
